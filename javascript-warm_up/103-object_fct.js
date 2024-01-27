a93ef7cb19df604e860d3d0ef678642adae5fe11
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-28 00:47:38 +0100

    Update 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
 // Salama-Jamal (102-add_me_maybe.js)
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-myObject.incr = function () {
-  this.value++;
+  type: 'object',
+  value: 12
 };
 
+// Define the incr function as a property on the myObject object
+Object.defineProperty(myObject, 'incr', {
+  value: function () {
+    // Increment the value property by 1
+    this.value += 1;
+  },
+  enumerable: false, // To avoid listing the incr function when logging the object
+  writable: true,
+  configurable: true
+});
+
+console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
