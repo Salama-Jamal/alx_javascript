09eed07fcab914f3fa55c4ab0e6dbfc76175c588
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-28 01:18:26 +0100

    Update 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
 // Salama-Jamal (103-object_fct.js)
+
 const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-
-myObject.incr = function() {
-  myObject.value += 1;
+  type: "object",
+  value: 12,
 };
 
-myObject.incr();
 console.log(myObject);
-myObject.incr();
-console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 
-function formatObject(obj) {
-  const formattedObject = JSON.parse(JSON.stringify(obj));
-  if (formattedObject.incr && formattedObject.incr instanceof Function) {
-    formattedObject.incr = '[Function]';
-  }
-  return formattedObject;
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
 }
