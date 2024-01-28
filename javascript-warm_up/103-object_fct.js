a4b756ef8c1ccca108f631aad3175c8a65b2f842
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-28 01:01:58 +0100

    Update 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 // Salama-Jamal (103-object_fct.js
 const myObject = {
-  type: 'object',
-  value: 12
+  type: "object",
+  value: 12,
 };
 console.log(myObject);
 
+// Add the "incr" method to the "myObject" object.
 myObject.incr = function () {
-    this.value += 1;
-}
+  if (typeof this.value === "number") {
+    this.value++;
+  }
+};
 
 myObject.incr();
 console.log(myObject);
@@ -16,29 +19,3 @@ myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
-// Update this script by adding a new function incr that increments the integer value.
-// You are not allowed to use var
-// guillaume@ubuntu:~/$ cat 103-object_fct.js
-// #!/usr/bin/node
-// const myObject = {
-//   type: 'object',
-//   value: 12
-// };
-// console.log(myObject);
-// /*
-// YOUR CODE HERE
-// */
-// myObject.incr();
-// console.log(myObject);
-// myObject.incr();
-// console.log(myObject);
-// myObject.incr();
-// console.log(myObject);
-
-// guillaume@ubuntu:~/$ ./103-object_fct.js 
-// { type: 'object', value: 12 }
-// { type: 'object', value: 13, incr: [Function] }
-// { type: 'object', value: 14, incr: [Function] }
-// { type: 'object', value: 15, incr: [Function] }
-// guillaume@ubuntu:~/$ 
